9536f9ff7553a8d076beee10c78b282204285a73
freemile2ru <sofiyullahj@gmail.com>
2023-06-24 08:48:53 +0100

    css changes

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -2,12 +2,36 @@ import React, { useState } from "react";
 
 const Projects = () => {
   const [webProjects] = useState([
-    { url: "/img/web_projects/unplugdd_home.png", title: "UNPLUGDD" },
-    { url: "/img/web_projects/smartBits.png", title: "THE SMARTBITS" },
-    { url: "/img/web_projects/Enterprise.png", title: "ENTERPRISE" },
-    { url: "/img/web_projects/AGS.png", title: "ACCELERATED GROWTH SOLUTIONS" },
-    { url: "/img/web_projects/Alamo.png", title: "ALAMO" },
-    { url: "/img/web_projects/Fiixy.png", title: "FIIXY" },
+    {
+      url: "/img/web_projects/unplugdd_home.png",
+      title: "UNPLUGDD",
+      tags: ["reactJS", "css", "nextJS", "bootstrap"],
+    },
+    {
+      url: "/img/web_projects/smartBits.png",
+      title: "THE SMARTBITS",
+      tags: ["scss", "html5", "javascript", 'api'],
+    },
+    {
+      url: "/img/web_projects/Enterprise.png",
+      title: "ENTERPRISE",
+      tags: ["html", "css", "jquery", 'core-ui', 'javascript'],
+    },
+    {
+      url: "/img/web_projects/AGS.png",
+      title: "ACCELERATED GROWTH SOLUTIONS",
+      tags: ["html5", "css3", "javascript", "jquery"],
+    },
+    {
+      url: "/img/web_projects/Alamo.png",
+      title: "ALAMO",
+      tags: ["html", "css", "javascript"],
+    },
+    {
+      url: "/img/web_projects/Fiixy.png",
+      title: "FIIXY",
+      tags: ["reactJS", "nodeJS", "mongoDB", "css"],
+    },
   ]);
 
   const [designProjects] = useState([
@@ -22,12 +46,17 @@ const Projects = () => {
       <h4 id="web_projects">WEB PROJECTS</h4>
       <div className="projects">
         {webProjects.map((m, i) => {
-          const { url, title } = m;
+          const { url, title, tags } = m;
           return (
             <div key={i} className="inner">
               <img src={url} alt="project" />
               <div className="overlay">
                 <h5>{title}</h5>
+              </div>
+              <div className="tags">
+                {tags.map((t, index) => (
+                  <span key={index}>{t}</span>
+                ))}
               </div>
             </div>
           );
