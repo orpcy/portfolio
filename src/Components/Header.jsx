1c6e7e0fbf3a81bea6d5680a6da22a69dd7664f0
freemile2ru <sofiyullahj@gmail.com>
2023-06-26 08:02:14 +0100

    add babel devDependency

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,21 +8,21 @@ const Header = () => {
   const handleMenuClose = () => setMenuToggle(false);
 
   const downloadResume = () => {
-    // if (window.confirm("Download SofiyullahJ Resume?")) {
-    //   // using Java Script method to get PDF file
-    //   fetch("/img/Resume.pdf").then((response) => {
-    //     response.blob().then((blob) => {
-    //       // Creating new object of PDF file
-    //       const fileURL = window.URL.createObjectURL(blob);
-    //       // Setting various property values
-    //       let alink = document.createElement("a");
-    //       alink.href = fileURL;
-    //       alink.download = "SofiyullahJ Resume.pdf";
-    //       alink.click();
-    //       handleMenuClose();
-    //     });
-    //   });
-    // }
+    if (window.confirm("Download SofiyullahJ Resume?")) {
+      // using Java Script method to get PDF file
+      fetch("/img/Resume.pdf").then((response) => {
+        response.blob().then((blob) => {
+          // Creating new object of PDF file
+          const fileURL = window.URL.createObjectURL(blob);
+          // Setting various property values
+          let alink = document.createElement("a");
+          alink.href = fileURL;
+          alink.download = "SofiyullahJ Resume.pdf";
+          alink.click();
+          handleMenuClose();
+        });
+      });
+    }
   };
 
   return (
