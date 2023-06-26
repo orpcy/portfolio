ffb681dcdf0c2272840bd7dae2e6a4fa2652f3fd
freemile2ru <sofiyullahj@gmail.com>
2023-06-26 16:39:29 +0100

    add codementor

--- src/Components/Socials.jsx
+++ src/Components/Socials.jsx
@@ -4,32 +4,41 @@ import { FaLinkedinIn, FaStackOverflow } from "react-icons/fa";
 
 const Socials = () => {
   return (
-    <div className="socials">
-      <a
-        target="_blank"
-        rel="noreferrer"
-        href="https://www.linkedin.com/in/sofiyullahj/"
-      >
-        <FaLinkedinIn size={18} />
-      </a>
-      <a target="_blank" rel="noreferrer" href="https://github.com/orpcy">
-        <AiFillGithub size={18} />
-      </a>
-      <a
-        target="_blank"
-        rel="noreferrer"
-        href="https://stackoverflow.com/users/10930895/orpcy"
-      >
-        <FaStackOverflow size={18} />
-      </a>
-      <a
-        target="_blank"
-        rel="noreferrer"
-        href="mailto:sofiyullahj@gmail.com"
-      >
-        <AiOutlineMail size={18} />
-      </a>
-      <hr />
+    <div>
+      <div className="socials">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/sofiyullahj/"
+        >
+          <FaLinkedinIn size={18} />
+        </a>
+        <a target="_blank" rel="noreferrer" href="https://github.com/orpcy">
+          <AiFillGithub size={18} />
+        </a>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://stackoverflow.com/users/10930895/orpcy"
+        >
+          <FaStackOverflow size={18} />
+        </a>
+        <a target="_blank" rel="noreferrer" href="mailto:sofiyullahj@gmail.com">
+          <AiOutlineMail size={18} />
+        </a>
+        <hr />
+      </div>
+      <div className="codementor">
+        <a
+          href="https://www.codementor.io/@sofiyullahj?refer=badge"
+          target="_blank"
+        >
+          <img
+            src="https://www.codementor.io/m-badges/sofiyullahj/find-me-on-cm-b.svg"
+            alt="Codementor badge"
+          />
+        </a>
+      </div>
     </div>
   );
 };
